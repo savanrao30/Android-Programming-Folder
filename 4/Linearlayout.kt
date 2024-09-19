d7a074a592bec143f8a45355b4d92ad712f560db
savanrao30 <134666605+savanrao30@users.noreply.github.com>
2024-09-18 21:28:16 -0500

    Create Linearlayout.kt

---
+++ 4/Linearlayout.kt
@@ -0,0 +1,12 @@
+package com.jfdimarzio.homework4by2784780
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LinearLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
