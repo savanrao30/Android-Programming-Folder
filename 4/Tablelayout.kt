ac873aed28a8886311d8ff884b48a50d2f5785e7
savanrao30 <134666605+savanrao30@users.noreply.github.com>
2024-09-18 21:32:52 -0500

    Create Tablelayout.kt

---
+++ 4/Tablelayout.kt
@@ -0,0 +1,12 @@
+package com.jfdimarzio.homework4by2784780
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TableLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.table_layout)
+    }
+}
