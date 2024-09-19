26faa25df897d141eb274e33fc729d770991ea56
savanrao30 <134666605+savanrao30@users.noreply.github.com>
2024-09-18 21:28:40 -0500

    Create LinearLayout.java

---
+++ 4/LinearLayout.java
@@ -0,0 +1,14 @@
+package com.jfdimarzio.homework4by2784780;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+    }
+}
